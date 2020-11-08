ef88367ec61eb9a93b1144ea45741be6baebd711
shagane <makiyansh@gmail.com>
2020-11-08 12:18:27 +0200

    divided columns into blocks

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,27 @@ from flask import Flask,request,render_template,jsonify
 from app import app
 import codecs
 import json
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-    with codecs.open(r"D:\Shagane\PATARAG\app\patarag-text\arm.txt", 'r', encoding="utf-8") as arm:
-        armtext = arm.readlines()
+    armtext = []
+    a_path = os.path.curdir + r'\app\patarag-text\a'
+    for i in sorted(os.listdir(a_path)):
+        with codecs.open(os.path.join(a_path, i), 'r', encoding="utf-8") as arm:
+            armtext.append(arm.readlines())
 
+    trltext = []
+    t_path = os.path.curdir + r'\app\patarag-text\t'
+    for i in sorted(os.listdir(t_path)):
+        with codecs.open(os.path.join(t_path, i), 'r', encoding="utf-8") as trl:
+            trltext.append(trl.readlines())
 
-    with open(r"D:\Shagane\PATARAG\app\patarag-text\rus.txt", 'r', encoding="utf-8") as rus:
-        rustext = rus.readlines()
-
-    with open(r"D:\Shagane\PATARAG\app\patarag-text\translit.txt", 'r', encoding="utf-8") as trl:
-        trltext = trl.readlines()
+    rustext = []
+    r_path = os.path.curdir + r'\app\patarag-text\r'
+    for i in sorted(os.listdir(r_path)):
+        with codecs.open(os.path.join(r_path, i), 'r', encoding="utf-8") as rus:
+            rustext.append(rus.readlines())
 
     return render_template('index.html', title='Home', armtext=armtext, trltext=trltext, rustext=rustext)
