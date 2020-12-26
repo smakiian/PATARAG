ff7d757bba710e1ce32f8592a2a6ff2b98897811
shagane <makiyansh@gmail.com>
2020-12-26 23:23:10 +0200

    added light theme

--- app/routes.py
+++ app/routes.py
@@ -24,3 +24,18 @@ def liturgia():
                 text_dict[j].append(f.readlines())
 
     return render_template('liturgia2.html', title='Liturgia', armtext=text_dict['a'], trltext=text_dict['t'], rustext=text_dict['r'])
+
+@app.route('/liturgiadark')
+def liturgia_dark():
+    text_dict = {'a': [], 't': [], 'r': []}
+    # "a" for armenian text
+    # "t" for translitaration of armenian
+    # "r" for russian text
+
+    for j in text_dict:
+        path = os.path.normpath(os.getcwd() + f"/app/patarag-text/{j}")
+        for i in sorted(os.listdir(path)):
+            with codecs.open(os.path.join(path, i), "r", encoding="utf-8") as f:
+                text_dict[j].append(f.readlines())
+
+    return render_template('liturgia-dark.html', title='Liturgia', armtext=text_dict['a'], trltext=text_dict['t'], rustext=text_dict['r'])
